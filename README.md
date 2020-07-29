cc86520153a62f1608f6d7b935fc33d8cfeaab31
sironekotoro <develop@sironekotoro.com>
2020-07-29 15:40:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Zengin::Perl - It's new $module
+
+# SYNOPSIS
+
+    use Zengin::Perl;
+
+# DESCRIPTION
+
+Zengin::Perl is ...
+
+# LICENSE
+
+Copyright (C) $GIT\_AUTHOR\_NAME.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+$GIT\_AUTHOR\_NAME &lt;$GIT\_AUTHOR\_EMAIL>
---
+++ lib/Zengin/Perl.pm
@@ -0,0 +1,39 @@
+package Zengin::Perl;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Zengin::Perl - It's new $module
+
+=head1 SYNOPSIS
+
+    use Zengin::Perl;
+
+=head1 DESCRIPTION
+
+Zengin::Perl is ...
+
+=head1 LICENSE
+
+Copyright (C) $GIT_AUTHOR_NAME.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+$GIT_AUTHOR_NAME E<lt>$GIT_AUTHOR_EMAILE<gt>
+
+=cut
+

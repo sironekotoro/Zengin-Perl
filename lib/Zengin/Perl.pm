e91a1b20965a1e4a305e6bad668fda92a91a1afe
sironekotoro <develop@sironekotoro.com>
2020-11-12 11:02:08 +0900

    update 0.10.20201112

--- lib/Zengin/Perl.pm
+++ lib/Zengin/Perl.pm
@@ -10,7 +10,7 @@ package Zengin::Perl {
     use Mouse::Util::TypeConstraints;
     use Smart::Args 0.14;
 
-    our $VERSION = "0.10.20201028";
+    our $VERSION = "0.10.20201112";
 
     has banks_file => (
         is      => "ro",
